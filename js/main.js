844258d225c9f3b7da4aba3ed642f396bf261e47
Alprames <thhussr@gmail.com>
2020-12-01 12:08:26 -0400

    Creates messages variable which contains the three possible outcomes of a game

--- js/main.js
+++ js/main.js
@@ -5,11 +5,14 @@ function randomChoice() {
     return choice;
 }
 
+let messages = [
+    {win: `Nice, you've won and that's because ${playerSelection} beats ${computerSelection}, congrats`},
+    {tie: `Well, you've both chosen ${playerSelection} so it seems that there's a tie between you two, maybe try again`},
+    {lose: `Oh noes, the computer has beaten you with ${computerSelection} against your ${playerSelection}`}
+]
+
 function play(playerSelection, computerSelection = randomChoice()) {
     playerSelection = prompt('And now, give me your choice');
-    let winMessage = `Nice, you've won and that's because ${playerSelection} beats ${computerSelection}, congrats`;
-    let loseMessage = `Oh noes, the computer has beaten you with ${computerSelection} against your ${playerSelection}`;
-    let tieMessage = `Well, you've both chosen ${playerSelection} so it seems that there's a tie between you two, maybe try again` 
     if (playerSelection === options[2] && computerSelection === options[1] || playerSelection === options[1] && computerSelection === options[0] || playerSelection === options[0] && computerSelection === options[2]) {
         console.log(winMessage);
     } else if (playerSelection === computerSelection) {
