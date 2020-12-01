4fa906d3b84e304be56e9349ecf97bf24f6a3512
Alprames <thhussr@gmail.com>
2020-12-01 12:15:36 -0400

    Try to apply functional programming paradigm (fails)

--- js/main.js
+++ js/main.js
@@ -9,16 +9,14 @@ let messages = [
     {win: `Nice, you've won and that's because ${playerSelection} beats ${computerSelection}, congrats`},
     {tie: `Well, you've both chosen ${playerSelection} so it seems that there's a tie between you two, maybe try again`},
     {lose: `Oh noes, the computer has beaten you with ${computerSelection} against your ${playerSelection}`}
-]
-
+];
+let afterMatchResult = () => {
+    let result = playerSelection === options[2] && computerSelection === options[1] || playerSelection === options[1] && computerSelection === options[0] || playerSelection === options[0] && computerSelection === options[2] ? messages.win :
+    playerSelection === computerSelection ? messages.tie : messages.lose;
+    return result;
+}
 function play(playerSelection, computerSelection = randomChoice()) {
-    playerSelection = prompt('And now, give me your choice');
-    if (playerSelection === options[2] && computerSelection === options[1] || playerSelection === options[1] && computerSelection === options[0] || playerSelection === options[0] && computerSelection === options[2]) {
-        console.log(winMessage);
-    } else if (playerSelection === computerSelection) {
-        console.log(tieMessage); 
-    } else {
-        console.log(loseMessage);
-    }
+    playerSelection = prompt('Select your stuff');
+    return afterMatchResult();
 }
 play();
